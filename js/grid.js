758f2e6b07e0feed4ef3f41be388a67862875e2c
nickng <nicknguyen0506@gmail.com>
2022-01-11 19:19:22 -0800

    add background to grids and remove container for more dynamic sizing

--- js/grid.js
+++ js/grid.js
@@ -1,17 +1,18 @@
-var Num_COLS = 6;
+var NUM_COLS = 6;
 var NUM_ROWS = 5;
 
 document.addEventListener('DOMContentLoaded', createGrid);
 
 function createGrid() {
-    const grids = document.querySelector("#container");
-    for (var i = 0; i < Num_COLS; i++) {
+    const body = document.querySelector("body");
+    for (var i = 0; i < NUM_COLS; i++) {
         for (var j = 0; j < NUM_ROWS; j++) {
-            let grid = document.createElement('div')
+            let grid = document.createElement('div');
             grid.id = "grid";
-            grid.style.width = 560/Num_COLS + 'px';
+            grid.style.width = 560/NUM_COLS + 'px';
             grid.style.height = 500/NUM_ROWS + 'px';
-            grids.appendChild(grid);
+            body.appendChild(grid);
         };
     };
 }
+
