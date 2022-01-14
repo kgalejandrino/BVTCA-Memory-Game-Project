db2943b79c339705661972dcc0665d80417d7a0c
nickng <nicknguyen0506@gmail.com>
2022-01-13 18:22:46 -0800

    even number of grid on each rows

--- js/grid.js
+++ js/grid.js
@@ -4,7 +4,7 @@ var NUM_ROWS = 5;
 document.addEventListener('DOMContentLoaded', createGrid);
 
 function createGrid() {
-    const body = document.querySelector("body");
+    const body = document.querySelector("#container");
     for (var i = 0; i < NUM_COLS; i++) {
         for (var j = 0; j < NUM_ROWS; j++) {
             let grid = document.createElement('div');
