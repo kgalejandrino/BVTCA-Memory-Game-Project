fdae94accf463304957ee610f92d0294660e9461
Kevin Alejandrino <kgalejandrino@gmail.com>
2022-01-18 18:04:48 -0800

    Updated code structure to oop

--- js/grid.js
+++ js/grid.js
@@ -4,7 +4,7 @@ var NUM_ROWS = 5;
 document.addEventListener('DOMContentLoaded', createGrid);
 
 function createGrid() {
-    const body = document.querySelector("#container");
+    const grid = document.querySelector("#grid");
     for (var i = 0; i < NUM_COLS; i++) {
         for (var j = 0; j < NUM_ROWS; j++) {
             let flipBox = document.createElement('div');
@@ -24,11 +24,11 @@ function createGrid() {
             flipBox.appendChild(flipBoxInner);
             flipBoxInner.appendChild(flipBoxFront);
             flipBoxInner.appendChild(flipBoxBack);
-            body.appendChild(flipBox);
+            grid.appendChild(flipBox);
         };
     };
 }
 
-document.querySelector("#container").addEventListener('click', (event) => {
+// document.querySelector("#container").addEventListener('click', (event) => {
 
-});
+// });
--- js/script.js
+++ js/script.js
@@ -1,51 +1,112 @@
-/* Variables */
-const bgMusic = document.querySelector('audio');
-const musicIcon = document.querySelector('.icon-sound');
-const loadingPercent = document.getElementById('percent');
-const playIcon = document.getElementById('icon-play');
-const menuOption = document.getElementById('menu-option');
-const loadingUI = document.getElementById('loading');
-
-let percent = 0;
-let playing = false;
-
-/* Function: Randomly increment percentage from 0 to 100% */
-const loading = () => {
-    let timer = setInterval(() => {
-        let randomNum = Math.floor(Math.random() * 20) + 1;
-        percent += randomNum;
-        if(percent > 100) {
-            percent = 100;
-            playIcon.style.animation = 'beat .35s infinite alternate';
-            playIcon.style.cursor = 'pointer';
-            playIcon.style.pointerEvents = 'auto';
-            clearInterval(timer);
+const gameController = (() => {
+    class Game {
+        constructor(col, row) {
+            this.col = col;
+            this.row = row;
         }
-        loadingPercent.innerHTML = percent;
-    }, 300)
-}
 
-/* Event: Click to open menu */
-playIcon.addEventListener('click', () => {
-    loadingUI.style.display = 'none';
-    menuOption.style.display = 'block';
-    bgMusic.play();
-    playing = true;
-})
-
-/* Event: Click to mute background music */
-musicIcon.addEventListener('click', () => {
-    if(playing) {
-        playing = false;
-        musicIcon.innerHTML = 'music_off';
-        bgMusic.pause()
-    } else {
-        playing = true; 
-        musicIcon.innerHTML = 'music_note';
-        bgMusic.play();
+        print = () => {
+            console.log(this.col, this.row);
+        }
     }
-});
+    return {
+        addGrid: () => {
+            
+        }
+    }
+})();
 
-loading();
+
+const UIController = (() => {
+    let percent = 0;
+    let playing = false;
+
+    const DOMstrings = {
+        bgIcon: '.icon-bg',
+        playIcon: '.icon-play',
+    }
+
+    return {
+        loading: () => {
+            let timer = setInterval(() => {
+                let randomNum = Math.floor(Math.random() * 20) + 1;
+                percent += randomNum;
+                if(percent > 100) {
+                    percent = 100;
+                    document.querySelector(DOMstrings.playIcon).style.animation = 'beat .35s infinite alternate';
+                    document.querySelector(DOMstrings.playIcon).style.cursor = 'pointer';
+                    document.querySelector(DOMstrings.playIcon).style.pointerEvents = 'auto';
+                    clearInterval(timer);
+                }
+                document.querySelector('.percent').innerHTML = percent;
+            }, 300);
+        },
+
+        displayMainMenu: () => {
+            document.querySelector('.loading').style.display = 'none';
+            document.querySelector('.menu').style.display = 'block';
+            document.querySelector(DOMstrings.bgIcon).style.display = 'block';
+            document.querySelector('.background-music').play();
+            playing = true;
+        },
+
+        toggleMusicBackground: () => {
+            if(playing) {
+                playing = false;
+                document.querySelector(DOMstrings.bgIcon).innerHTML = 'music_off';
+                document.querySelector('.background-music').pause();
+            } else {
+                playing = true; 
+                document.querySelector(DOMstrings.bgIcon).innerHTML = 'music_note';
+                document.querySelector('.background-music').play();
+            }
+        },
+
+        // getLevel: (event) => {
+        //     let level = event.target.dataset.level;
+        //     if(level === 'easy') {
+        //         return {
+        //             col: 4,
+        //             row: 4,
+        //         }
+        //     } else if(level === 'medium') {
+        //         return {
+        //             col: 5,
+        //             row: 5,
+        //         }
+        //     } else if(level === 'hard') {
+        //         return {
+        //             col: 6,
+        //             row: 6,
+        //         }
+        //     }
+        // },
+
+        getDOMstrings: () => {
+            return DOMstrings;
+        }
+    }
+})();
+
+const appController = ((UICtrl) => {
+    const setupEventListener = () => {
+        const DOM = UICtrl.getDOMstrings();
+
+        UICtrl.loading();
+        document.querySelector(DOM.playIcon).addEventListener('click', UICtrl.displayMainMenu);
+        document.querySelector(DOM.bgIcon).addEventListener('click', UICtrl.toggleMusicBackground);
+        document.querySelector('.option').addEventListener('click', (event) => UICtrl.getLevel(event));
+    }
+
+    return {
+        init: () => {
+            setupEventListener();
+        }
+    }
+})(UIController);
+
+appController.init();
+
+
 
 
